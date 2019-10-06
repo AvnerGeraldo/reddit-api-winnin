4b9f480c363ea315d1f26a2ff6660be64a8e6fa6
Avner <avner.conceicao@hotmail.com>
2019-10-06 16:49:24 -0300

    Add 'border-color' em Box component

--- src/components/styled-components/Box.js
+++ src/components/styled-components/Box.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const Box = styled.div`
     background-color: ${props => props.backgroundColor || "#FFF"};
+    border-color: ${props => props.borderColor | "none"};
     color: ${props => props.color || "#FFF"};
 `
 export default Box
--- src/components/styled-components/Header.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const Header = styled.div`
-    background-color: #000;
-    color: #FFF;
-    padding: 0 5px;
-`
-export default Header

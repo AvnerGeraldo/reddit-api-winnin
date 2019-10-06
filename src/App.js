bf74c6044496c01d7328b350b6c23d7f31744003
Avner <avner.conceicao@hotmail.com>
2019-10-06 16:15:38 -0300

    Iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Winnin
+      </header>
+      <div className="App-sort-list">
+        Buttons(Sort List)
+      </div>
+      <div className="Post-List">
+        Post List
+        <button>More Posts</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/styled-components/Header.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Header = styled.div`
+    background-color: #000;
+    color: #FFF;
+    padding: 0 5px;
+`
+export default Header

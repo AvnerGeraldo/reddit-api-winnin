ca73412f51a6c13a039592e01a1441759d7b8733
Avner <avner.conceicao@hotmail.com>
2019-10-08 00:21:53 -0300

    Add assets

--- src/components/styled-components/Button.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Button = styled.button`
-    background-color: ${props => props.backgroundColor | "#FFF"};
-    border-color: ${props => props.borderColor | "none"};
-    color: ${props => props.color | "#000"}
-`
-
-export default Button
